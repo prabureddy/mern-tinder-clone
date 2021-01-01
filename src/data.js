9fa5f636840258e17ed0a913f2d673447ec3af64
Bhargava Prabu Reddy M <prabu0reddy777@gmail.com>
2021-01-01 20:23:21 +0530

    updated

--- src/data.js
+++ src/data.js
@@ -10,4 +10,16 @@ export default [
     imgURL:
       "https://cdn.britannica.com/56/199056-050-CCC44482/Jeff-Bezos-2017.jpg",
   },
+  {
+    id: "3",
+    name: "Sundar Pichai",
+    imgURL:
+      "https://pbs.twimg.com/profile_images/864282616597405701/M-FEJMZ0_400x400.jpg",
+  },
+  {
+    id: "4",
+    name: "Tim Cook",
+    imgURL:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Tim_Cook_%282017%2C_cropped%29.jpg/1200px-Tim_Cook_%282017%2C_cropped%29.jpg",
+  },
 ];
